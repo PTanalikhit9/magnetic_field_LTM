b51fe65aeed518d2c9311145ddfad4d55c42cfea
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-17 21:45:46 +0700

    Create magnetic_field_3_B_and_V_with_magnet_boundary.py

---
+++ magnetic_field_3_B_and_V_with_magnet_boundary.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the dimensions of the rectangular magnet
+length = 10.0
+height = 4.0
+
+# Define the strength of the magnetization
+magnetization = 1.0
