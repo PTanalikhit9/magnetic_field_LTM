38af996fe3bda9673ef4d3db7ee4f9258bbc22ef
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-19 22:16:30 +0700

    Create ring_magnet.py

---
+++ ring_magnet.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the dimensions of the circular ring magnet
+outer_radius = 5.0
+inner_radius = 4.0
+height = 4.0
+
+# Define the strength of the magnetization
+magnetization = 1.0
+
+# Define the grid for the magnetic field and potential calculations
+x = np.linspace(-15, 15, 100)
+z = np.linspace(-15, 15, 100)
+X, Z = np.meshgrid(x, z)
+
+# Initialize the magnetic field and potential arrays
+Bx = np.zeros_like(X)
+Bz = np.zeros_like(Z)
+V = np.zeros_like(X)
+
+# Number of charges on the magnet's surface
+num_charges = 1000
