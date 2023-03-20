0ec533d4331fdabf67009c00a1aa423157292911
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-20 20:59:16 +0700

    Update ring_magnet2.py

--- ring_magnet2.py
+++ ring_magnet2.py
@@ -19,3 +19,31 @@ X, Z = np.meshgrid(x, z)
 Bx = np.zeros_like(X)
 Bz = np.zeros_like(Z)
 V = np.zeros_like(X)
+
+# Number of charges on the magnet's surface
+num_charges = 1000
+
+# Calculate the magnetic field and potential at each grid point
+for i in range(len(x)):
+    for j in range(len(z)):
+        for k in range(num_charges):
+            # Calculate the angle of the charges
+            angle = k * 2 * np.pi / num_charges
+
+            # Calculate the position of the charges
+            charge_x = outer_radius * np.cos(angle)
+            charge_z_north = outer_radius * np.sin(angle) + height / 2
+            charge_z_south = outer_radius * np.sin(angle) - height / 2
+
+            # Calculate the distances from the charges
+            dx = x[i] - charge_x
+            dz_north = z[j] - charge_z_north
+            dz_south = z[j] - charge_z_south
+
+            # Magnetic field components
+            Bx[i, j] += magnetization / num_charges * (dz_south / (dx ** 2 + dz_south ** 2) - dz_north / (dx ** 2 + dz_north ** 2))
+            Bz[i, j] += magnetization / num_charges * (dx / (dx ** 2 + dz_south ** 2) - dx / (dx ** 2 + dz_north ** 2))
+
+            # Magnetic potential
+            V[i, j] += magnetization / num_charges * (np.log(np.sqrt(dx ** 2 + dz_south ** 2)) - np.log(np.sqrt(dx ** 2 + dz_north ** 2)))
+
