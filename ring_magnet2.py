eea740feae234deb2b351fd3e00d3cccc60738ad
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-20 20:58:37 +0700

    Update ring_magnet2.py

--- ring_magnet2.py
+++ ring_magnet2.py
@@ -6,3 +6,11 @@ import matplotlib.pyplot as plt
 outer_radius = 5.0
 inner_radius = 4.0
 height = 4.0
+
+# Define the strength of the magnetization
+magnetization = 1.0
+
+# Define the grid for the magnetic field and potential calculations
+x = np.linspace(-15, 15, 100)
+z = np.linspace(-15, 15, 100)
+X, Z = np.meshgrid(x, z)
