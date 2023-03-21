588fb233cbcfcc1d173ad71db00d82c1e7c761df
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-21 22:59:13 +0700

    Update ring3.py

--- ring3.py
+++ ring3.py
@@ -22,3 +22,36 @@ V = np.zeros_like(X)
 
 # Number of charges on the magnet's surface
 num_charges = 1000
+
+# Calculate the magnetic field and potential at each grid point
+for i in range(len(x)):
+    for j in range(len(z)):
+        for k in range(num_charges):
+            # Calculate the angle of the charges
+            angle = k * 2 * np.pi / num_charges
+
+            # Calculate the position of the charges
+            charge_x_north = outer_radius * np.sin(angle) + height / 2
+            charge_x_south = outer_radius * np.sin(angle) - height / 2
+            charge_z = outer_radius * np.cos(angle)
+
+            # Calculate the distances from the charges
+            dx_north = x[i] - charge_x_north
+            dx_south = x[i] - charge_x_south
+            dz = z[j] - charge_z
+
+            # Magnetic field components
+            Bx[i, j] += magnetization / num_charges * (dz / (dx_north ** 2 + dz ** 2) - dz / (dx_south ** 2 + dz ** 2))
+            Bz[i, j] += magnetization / num_charges * (dx_north / (dx_north ** 2 + dz ** 2) - dx_south / (dx_south ** 2 + dz ** 2))
+
+            # Magnetic potential
+            V[i, j] += magnetization / num_charges * (np.log(np.sqrt(dx_north ** 2 + dz ** 2)) - np.log(np.sqrt(dx_south ** 2 + dz ** 2)))
+
+  
+# Create the plot
+fig, ax = plt.subplots()
+
+# Plot the magnetic potential as a colormap
+c = ax.pcolormesh(X, -Z, V, cmap='coolwarm', shading='auto')
+fig.colorbar(c, ax=ax, label='Magnetic Potential')
+
