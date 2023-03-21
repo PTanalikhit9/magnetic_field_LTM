31504095793fddf695fa925372b07d2aa8d19937
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-21 22:57:41 +0700

    Create ring3.py

---
+++ ring3.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the dimensions of the circular ring magnet
+outer_radius = 5.0
+inner_radius = 4.0
+height = 4.0
+
+# Define the strength of the magnetization
+magnetization = 1.0
