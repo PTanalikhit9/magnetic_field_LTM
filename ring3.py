355c38520be27ab578fd02f9551393fc97fcda76
Pattarapon Tanalikhit (Ohm, Pat) <56800120+PTanalikhit9@users.noreply.github.com>
2023-03-21 22:58:17 +0700

    Update ring3.py

--- ring3.py
+++ ring3.py
@@ -9,3 +9,16 @@ height = 4.0
 
 # Define the strength of the magnetization
 magnetization = 1.0
+
+# Define the grid for the magnetic field and potential calculations
+x = np.linspace(-15, 15, 100)
+z = np.linspace(-15, 15, 100)
+X, Z = np.meshgrid(x, z)
+
+# Initialize the magnetic field and potential arrays
+Bx = np.zeros_like(X)
+Bz = np.zeros_like(Z)
+V = np.zeros_like(X)
+
+# Number of charges on the magnet's surface
+num_charges = 1000
